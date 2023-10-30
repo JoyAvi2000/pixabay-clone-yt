f20701769940c46899a5ed06446d9b5f48f675e1
Avinaba Mazumdar <avinaba2@gmail.com>
2023-10-30 23:55:19 +0530

    setting up frontend

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Routes } from "react-router-dom";
+
+function App() {
+     return (
+          <div className='w-screen h-screen flex flex-col items-center justify-start'>
+               {/* Header */}
+
+               {/* main content */}
+               <main className='w-full h-full flex items-center justify-center'>
+                    {/* Routes */}
+                    <Routes></Routes>
+               </main>
+          </div>
+     );
+}
+
+export default App;
